e062753de5acbbd07aa1de693c5c0b259ced81d2
Bandhan-singh-katoch <parbhatbandhan@gmail.com>
2021-07-09 11:44:17 +0530

    Notes taking app...

--- app/src/main/java/com/example/notesapp/WordRoomDatabase.kt
+++ app/src/main/java/com/example/notesapp/WordRoomDatabase.kt
@@ -13,6 +13,7 @@ public abstract class WordRoomDatabase: RoomDatabase() {
     companion object{
 
         //...
+        //..
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
